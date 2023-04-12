e240a4f768b39f63b43db0f77ad3ccd3d1b4dfdd
Abdullah <abdullahkhrief@gmail.com>
2023-04-12 14:46:46 +0300

    CSS file has been completed

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import _ from 'lodash';
 import './style.css';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-   return element;
- }
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = '';
 
- document.body.appendChild(component());
+  return element;
+}
+
+document.body.appendChild(component());
